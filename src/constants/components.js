1afec3900e93cdb3a738e6a17279a29f91c88332
harshitchopra7 <chopraharshit2000@gmail.com>
2022-06-30 10:00:55 +0530

    signup screen created and code optimised

---
+++ src/constants/components.js
@@ -0,0 +1,8 @@
+export const SIGN_IN_BODY_TEXT = {
+    SIGN_IN: "Sign In",
+    EMAIL_PLACEHOLDER: "Enter your email",
+    PASSWORD_PLACEHOLDER: "Enter your password",
+    LOGIN_AS_GUEST_USER: "Login as Guest User",
+    NEW_TO_MOVIE_APP: "New to MovieApp?",
+    SIGN_UP_NOW: "SignUp Now."
+}
