3f15bc3b4ab3709dc9842b169b562ba61cb4b2ed
harshitchopra7 <chopraharshit2000@gmail.com>
2022-07-04 09:56:07 +0530

    row styling and general row component creted

--- src/components/HomePageContent/HomePageContent.js
+++ src/components/HomePageContent/HomePageContent.js
@@ -1,25 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import "./HomePageContent.css";
+import Row from "./Row/Row";
 
 function HomePageContent() {
+  // apiKey = "0477f5f21bb6f8f9d6406faeb32c0ef9"
 
-    // apiKey = "0477f5f21bb6f8f9d6406faeb32c0ef9"
+  // image path = https://image.tmdb.org/t/p/original
 
-    const [topRatedMovies, setTopRatedMovies] = useState([])
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
 
-    useEffect(() => {
-        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=0477f5f21bb6f8f9d6406faeb32c0ef9&language=en-US&page=1")
-        .then(res => res.json())
-        .then(data => setTopRatedMovies(data.results))
-        .catch(error => console.log(error))
-    }, [])
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=0477f5f21bb6f8f9d6406faeb32c0ef9&language=en-US&page=1"
+    )
+      .then((res) => res.json())
+      .then((data) => setTopRatedMovies(data.results))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <div>
-        {topRatedMovies.map((value, key) => (
-            <p>{value.title}</p>
-        ))}
+    <div className="homepagecontent_container">
+        <Row 
+            rowTitle="Top Rated Movies"
+            moviesArray={topRatedMovies}
+            posterPath={true}
+        />
+      {/* <div className="row_container">
+        <p className="row_container_title">Top Rated Movies</p>
+        <div className="movies_row_container">
+          {topRatedMovies.map((value, key) => (
+            <div className="movies_row">
+              <img
+                src={`https://image.tmdb.org/t/p/original${value.poster_path}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default HomePageContent
+export default HomePageContent;
