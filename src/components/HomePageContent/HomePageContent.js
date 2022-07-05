6c26db7cc944386ae0cf05c4b0f0909aedd645ac
harshitchopra7 <chopraharshit2000@gmail.com>
2022-07-05 09:52:59 +0530

    call 2 more apis and make it optimised

--- src/components/HomePageContent/HomePageContent.js
+++ src/components/HomePageContent/HomePageContent.js
@@ -3,40 +3,64 @@ import "./HomePageContent.css";
 import Row from "./Row/Row";
 
 function HomePageContent() {
-  // apiKey = "0477f5f21bb6f8f9d6406faeb32c0ef9"
-
   // image path = https://image.tmdb.org/t/p/original
 
+  // APIs   
+  // Popular Movies
+  // https://api.themoviedb.org/3/movie/popular?api_key=yourApiKey&language=en-US&page=1
+
+  // Now Playing
+  // https://api.themoviedb.org/3/movie/now_playing?api_key=yourApiKey&language=en-US&page=1
+
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "0477f5f21bb6f8f9d6406faeb32c0ef9";
+
+  const topRatedMoviesApiUrl = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+  const popularMoviesApiUrl = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+  const nowPlayingMoviesApiUrl = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&p`;
+
   const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+
+  function callApi(url, setVariable) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setVariable(data.results))
+      .catch((err) => console.log(err));
+  }
 
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?api_key=0477f5f21bb6f8f9d6406faeb32c0ef9&language=en-US&page=1"
-    )
-      .then((res) => res.json())
-      .then((data) => setTopRatedMovies(data.results))
-      .catch((error) => console.log(error));
+    callApi(topRatedMoviesApiUrl, setTopRatedMovies);
+    callApi(popularMoviesApiUrl, setPopularMovies);
+    callApi(nowPlayingMoviesApiUrl, setNowPlayingMovies);
   }, []);
 
   return (
     <div className="homepagecontent_container">
-        <Row 
-            rowTitle="Top Rated Movies"
-            moviesArray={topRatedMovies}
-            posterPath={true}
+      {topRatedMovies.length > 0 && (
+        <Row
+          rowTitle="Top Rated Movies"
+          moviesArray={topRatedMovies}
+          posterPath={true}
         />
-      {/* <div className="row_container">
-        <p className="row_container_title">Top Rated Movies</p>
-        <div className="movies_row_container">
-          {topRatedMovies.map((value, key) => (
-            <div className="movies_row">
-              <img
-                src={`https://image.tmdb.org/t/p/original${value.poster_path}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div> */}
+      )}
+
+      {popularMovies.length > 0 && (
+        <Row
+          rowTitle="Popular Movies"
+          moviesArray={popularMovies}
+          posterPath={true}
+        />
+      )}
+
+      {nowPlayingMovies.length > 0 && (
+        <Row
+          rowTitle="Now Playing Movies"
+          moviesArray={nowPlayingMovies}
+          posterPath={true}
+        />
+      )}
     </div>
   );
 }
